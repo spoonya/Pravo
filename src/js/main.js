50f7d43381668347e7923789c4df0f84c2d5d0ac
nomad8t <serega18t@tut.by>
2020-10-30 01:11:57 +0300

    feat: activities

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,33 @@
-const mySwiper = new Swiper('.swiper-container', {
+const swiperIntro = new Swiper('.swiper-container--intro', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 700,
+  autoplay: true,
+
+  navigation: {
+    nextEl: '.slider-btn-next',
+    prevEl: '.slider-btn-prev',
+  },
+});
+
+const swiperActivities = new Swiper('.swiper-container--activities', {
   direction: 'horizontal',
   loop: true,
   speed: 700,
 
   navigation: {
-    nextEl: '.intro__btn-next',
-    prevEl: '.intro__btn-prev',
+    nextEl: '.slider-btn-next',
+    prevEl: '.slider-btn-prev',
+  },
+});
+
+const swiperReviews = new Swiper('.swiper-container--reviews', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 700,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.slider-btn-next',
+    prevEl: '.slider-btn-prev',
   },
 });
